567f75930efcdec9f2fa0ac0e58844301b5e5abf
Alessandro <alecosta.web@gmail.com>
2018-03-01 16:09:32 -0300

    Controle de Licenciatura criada

---
+++ app/Http/Controllers/LicenciaturaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LicenciaturaController extends Controller
+{
+    //
+    public function mostrarAlunos() {
+        return view('licenciatura.mostraralunos');
+    }
+}
