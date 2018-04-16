8c69f439cbeec42aee6b39c0a80e11777b110f31
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2018-04-16 12:51:30 -0300

    Implementado jeroennoten/Laravel-AdminLTE, parte 4
    Adicionei as chamadas para o AdminLTE

--- resources/views/graduacao/busca.blade.php
+++ resources/views/graduacao/busca.blade.php
@@ -1,15 +1,25 @@
-<form method="post" action"/buscaReplicado">
-    {{ csrf_field() }} 
-    Nº USP: <input name="codpes" requrired>
+@extends('adminlte::page')
 
-    <button type="submit">Buscar</button>
-</form>
+@section('title', 'Buscar aluno')
 
-@if (isset($graduacaoCurso))
+@section('content_header')
+    <h1>Buscar aluno</h1>
+@stop</h>
 
-    <h2>{{ $graduacaoCurso['nompes'] }}</h2>
-    Curso: <strong>{{ $graduacaoCurso['codcur'] }} - {{ $graduacaoCurso['nomcur'] }}</strong><br />
-    Habilitação: <strong>{{ $graduacaoCurso['codhab'] }} - {{ $graduacaoCurso['nomhab'] }}</strong><br />
-    Ano de ingresso: <strong>{{ Carbon\Carbon::parse($graduacaoCurso['dtainivin'])->format('Y') }}</strong>
+@section('content')
+    <form method="post" action"/buscaReplicado">
+        {{ csrf_field() }} 
+        Nº USP: <input name="codpes" requrired>
 
-@endif
+        <button type="submit">Buscar</button>
+    </form>
+
+    @if (isset($graduacaoCurso))
+
+        <h2>{{ $graduacaoCurso['nompes'] }}</h2>
+        Curso: <strong>{{ $graduacaoCurso['codcur'] }} - {{ $graduacaoCurso['nomcur'] }}</strong><br />
+        Habilitação: <strong>{{ $graduacaoCurso['codhab'] }} - {{ $graduacaoCurso['nomhab'] }}</strong><br />
+        Ano de ingresso: <strong>{{ Carbon\Carbon::parse($graduacaoCurso['dtainivin'])->format('Y') }}</strong>
+
+    @endif
+@stop</p>
