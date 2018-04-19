cd07f99c14afb893b7d83bfbc307105730876940
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2018-04-19 16:32:44 -0300

    Revida linha do dd()

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -66,7 +66,6 @@ class LoginController extends Controller
 		if ( (!in_array($userSenhaUnica->id, $secretaria)) && (!$graduacao->verifica($userSenhaUnica->id, env('REPLICADO_CODUND'))) ) {
             # exibir mensagem flash de restrição...
             $msg = "Acesso restrito a secretaria do Serviço de Graduação e alunos ativos de Graduação desta unidade."; 
-            # dd('ACESSO RESTRITO');
             session()->flash('alert-danger', $msg);
             return redirect('/');
         }    
