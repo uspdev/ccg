8321f63bf194a2bfbc594f2b294d3b00a7186190
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-04-04 17:04:09 -0300

    Adicionada a coluna tipeqv
    Nas tabelas de DisciplinasObrigatoriasEquivalentes e DisciplinasLicenciaturasEquivalentes
    O valor do tipo de equivalência pode ser 'E' ou 'OU'

---
+++ database/migrations/2019_04_04_194919_add_tipeqv_to_disciplinas_obrigatorias_equivalentes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTipeqvToDisciplinasObrigatoriasEquivalentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('DisciplinasObrigatoriasEquivalentes', function($table) {
+            # Tipo de equivalência de disciplina
+            # 'E' ou 'OU'
+            $table->string('tipeqv')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('DisciplinasObrigatoriasEquivalentes', function($table) {
+            $table->dropColumn('tipeqv');
+        });
+    }
+}
---
+++ database/migrations/2019_04_04_195720_add_tipeqv_to_disciplinas_licenciaturas_equivalentes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTipeqvToDisciplinasLicenciaturasEquivalentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('DisciplinasLicenciaturasEquivalentes', function($table) {
+            # Tipo de equivalência de disciplina
+            # 'E' ou 'OU'
+            $table->string('tipeqv')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('DisciplinasLicenciaturasEquivalentes', function($table) {
+            $table->dropColumn('tipeqv');
+        });
+    }
+}
