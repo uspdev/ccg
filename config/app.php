3e07a181e81d941a83a8383ea4130c89e559fbf0
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-05-03 10:46:50 -0300

    Configurando laravel-snappy

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         JeroenNoten\LaravelAdminLte\ServiceProvider::class,
+        Barryvdh\Snappy\ServiceProvider::class,
 
         /*
          * Package Service Providers...
