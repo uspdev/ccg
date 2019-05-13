fb7857274eaefb0a0226bf6fafd8e6542f5e21c7
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-05-13 15:59:21 -0300

    Criada model e migration para Observacoes e Dispensas

---
+++ database/migrations/2019_05_13_174350_create_alunos_observacoes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlunosObservacoes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('AlunosObservacoes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_crl')->unsigned();
+            $table->integer('codpes');
+            $table->string('txtobs');            
+            $table->unique(array('codpes', 'id_crl'));            
+            $table->timestamps();
+
+            $table->foreign('id_crl')->references('id')->on('Curriculos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('AlunosObservacoes');
+    }
+}
